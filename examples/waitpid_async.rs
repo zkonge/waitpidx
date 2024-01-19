0f0ee1720d671e94f8e8062a932db5192a6de2c0
Konge <zkonge@outlook.com>
2024-01-20 05:56:49 +0800

    remove tokio macro feature

--- examples/waitpid_async.rs
+++ examples/waitpid_async.rs
@@ -1,11 +1,18 @@
 use std::env::args;
 
-#[tokio::main(flavor = "current_thread")]
-async fn main() {
+async fn amain() {
     let pid = args()
         .nth(1)
         .expect("specify a PID(TGID) to wait")
         .parse::<u32>()
         .expect("parse PID failed");
     waitpidx::waitpid_async(pid).await.unwrap();
+}
+
+fn main() {
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(amain())
 }
